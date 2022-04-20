7f1eb9e7c511e689e97a12ef62a5f65c5cc697a4
Calin Buzetelu <calinbuze@gmail.com>
2022-04-20 17:11:33 +0300

    Add new fortunes

--- src/main/java/com/example/entity/BasketballFortune.java
+++ src/main/java/com/example/entity/BasketballFortune.java
@@ -1,11 +1,30 @@
 package com.example.entity;
 
+import java.util.List;
+import java.util.Random;
+
 public class BasketballFortune implements Fortune {
 
+	private final List<String> fortunesList = List.of(
+			"You will get all the ankles!",
+			"May your floaters reach sky high!",
+			"Your 3 pointers shall always go in!"
+	);
+	private Random random = new Random();
 	private String fortune;
 
 	@Override
 	public String getFortune() {
+		int randomPosition = getRandomPosition();
+		setFortune(fortunesList.get(randomPosition));
 		return fortune;
+	}
+
+	public void setFortune(String fortune) {
+		this.fortune = fortune;
+	}
+
+	private int getRandomPosition() {
+		return random.nextInt(fortunesList.size());
 	}
 }
--- src/main/java/com/example/entity/SwimmingFortune.java
+++ src/main/java/com/example/entity/SwimmingFortune.java
@@ -1,11 +1,30 @@
 package com.example.entity;
 
+import java.util.List;
+import java.util.Random;
+
 public class SwimmingFortune implements Fortune {
 
+	private final List<String> fortunesList = List.of(
+			"May your freestyle be like lightning!",
+			"Water shall never go in your nose when you do the back stroke",
+			"Better train the dive!"
+	);
+	private Random random = new Random();
 	private String fortune;
 
 	@Override
 	public String getFortune() {
+		int randomPosition = getRandomPosition();
+		setFortune(fortunesList.get(randomPosition));
 		return fortune;
+	}
+
+	public void setFortune(String fortune) {
+		this.fortune = fortune;
+	}
+
+	private int getRandomPosition() {
+		return random.nextInt(fortunesList.size());
 	}
 }
